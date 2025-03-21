0b2dedb181531518311b536a4e44a69d3d89908d
brojor <bronislav.jordan@gmail.com>
2025-03-21 10:24:40 +0100

    chore(lint): add antfu eslint config
    - Install @antfu/eslint-config as dev dependency
    - Configure ESLint to use antfu's preset
    - Adjust VSCode settings to support the linting rules
    - Add linting scripts to package.json
    - Update Nuxt ESLint configuration
    - Add code style badge to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My personal website
+
+[![code style](https://antfu.me/badge-code-style.svg)](https://github.com/antfu/eslint-config)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts']
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
