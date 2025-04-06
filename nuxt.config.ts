1160ba298c4c36bf8d557f3a83fc56b8f0be7466
brojor <bronislav.jordan@gmail.com>
2025-04-06 16:07:52 +0200

    feat(sitemap): integrate sitemap support into content configuration and nuxt config
    - Add sitemap support to Czech and English content collections via `asSitemapCollection`
    - Update `nuxt.config.ts` to include sitemap module and define sources for sitemap generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
