143c64bc7f7725ecc461ea26c8005f7a7d36fefc
brojor <bronislav.jordan@gmail.com>
2025-04-18 19:58:02 +0200

    style(layout): use fixed header

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout class="flex flex-col min-h-screen px-4 sm:px-6">
+  <NuxtLayout class="flex flex-col min-h-screen px-4 sm:px-6 pt-[51px]">
     <NuxtPage class="flex-1" />
   </NuxtLayout>
 </template>
