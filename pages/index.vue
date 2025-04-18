2e4ee81e83fdd3815ac112fa248e17ebdb1dda6c
brojor <bronislav.jordan@gmail.com>
2025-04-18 18:45:39 +0200

    style(homepage): update layout and typography in index.vue
    - Adjusted main container padding and margins for better responsiveness.
    - Updated heading sizes and paragraph styles for improved readability.
    - Enhanced tech stack section layout with better spacing and alignment.
    - Added horizontal rule for visual separation in the tech stack section.

--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,35 @@ definePageMeta({
 </script>
 
 <template>
-  <main class="max-w-[65ch] mx-auto mt-12">
-    <section>
-      <h1 class="text-6xl font-bold">
+  <main class="max-w-[65ch] mx-auto px-4 sm:px-6">
+    <section class="my-16 md:my-32">
+      <h1 class="text-4xl md:text-6xl font-bold">
         {{ $t('landingPage.hi') }}, <br>
         {{ $t('landingPage.iAm') }} <BroJor />,
         <br>{{ $t('landingPage.webDeveloper') }}.
       </h1>
-      <p class="text-xl mt-5">
+      <p class="text-lg md:text-xl mt-4 md:mt-5">
         {{ $t('landingPage.description') }}
       </p>
     </section>
-    <section class="mt-20">
-      <h2 class="text-3xl font-semibold mb-10">
+    <section>
+      <h2 class="text-2xl md:text-3xl font-semibold">
         {{ $t('landingPage.techStack.title') }}
       </h2>
+      <hr class="border-accent-light/50 mt-3 md:mt-4 mb-6 md:mb-10">
 
-      <div v-for="(items, category) in techstack" :key="category" class="mt-8">
-        <h3 class="text-xl font-medium mb-5">
-          {{ $t(`landingPage.techStack.${category}`) }}
-        </h3>
-
-        <div class="flex flex-wrap gap-2">
-          <div v-for="item in items" :key="item.name">
-            <Icon :name="item.icon" size="4rem" />
+      <div class="space-y-10 md:space-y-12">
+        <div v-for="(items, category) in techstack" :key="category">
+          <h3 class="text-lg md:text-xl font-medium mb-4 md:mb-5">
+            {{ $t(`landingPage.techStack.${category}`) }}:
+          </h3>
+          <div class="flex flex-wrap gap-2 justify-center sm:justify-start">
+            <div v-for="item in items" :key="item.name" class="border border-white/20 w-[98px] md:w-[114px] py-3 md:py-4 rounded-lg">
+              <Icon :name="item.icon" class="mx-auto opacity-90 h-10 w-10 md:h-12 md:w-12" />
+              <p class="text-center text-xs md:text-sm mt-2 md:mt-3">
+                {{ item.name }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
