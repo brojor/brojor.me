7b9c6526a34b83fc9fd141be9d1bc59a5461f72e
brojor <bronislav.jordan@gmail.com>
2025-04-19 06:40:54 +0200

    fix(layout): prevent heading overlap when using anchor links
    - Updated scroll padding to align with header height.
    - Reduced header padding for a more compact design.
    - Implemented smooth scrolling behavior for better user experience.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout class="flex flex-col min-h-screen px-4 sm:px-6 pt-[51px]">
+  <NuxtLayout class="flex flex-col min-h-screen px-4 sm:px-6 pt-[46px]">
     <NuxtPage class="flex-1" />
   </NuxtLayout>
 </template>
